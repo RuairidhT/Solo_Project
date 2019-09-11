73fa308dff697a3b9f6e2998534342f4eb5d099e
RuairidhT <ruairidht2@gmail.com>
2019-09-11 19:43:57 +0100

    Read functionality added to the Exercises table

--- assets/js/Exercises.js
+++ assets/js/Exercises.js
@@ -1,4 +1,76 @@
-function eClicked(form){
+var xhr = new XMLHttpRequest();
+var json;
+var url = "http://34.89.83.113:9000/exercises";
+xhr.open("GET", url, true);
+xhr.setRequestHeader("Content-Type", "application/json");
+xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+        json = JSON.parse(xhr.responseText);
+        handleExercises(json)
+    }
+}
+
+xhr.send();
+
+
+function handleExercises(data) {
+
+    let exerciseInfo = [];
+    let exercisesInfo;
+
+    for (let ex of data) {// Each owner
+        exercisesInfo = [];
+
+        exercisesInfo.push(ex.id);
+        exercisesInfo.push(ex.name);
+        exercisesInfo.push(ex.muscleGroup);
+        exercisesInfo.push(ex.description);
+        exercisesInfo.push(ex.tutorial);
+
+        exerciseInfo.push(exercisesInfo);
+    }
+    console.log(exerciseInfo);
+
+    for (let exer of exerciseInfo) {
+        buildTable(exer);
+    }
+
+}
+
+function buildTable(tableData) {
+    
+    let tableBody = document.getElementById("tabBod");
+    let container;
+    let contInner;
+
+    container = document.createElement("tr");
+    tableBody.appendChild(container);
+
+    for (let data of tableData) {
+        contInner = document.createElement("td");
+        if(data.toString().includes("https://")){
+            data = "<a href="+data+">Tutorial</a>";
+        }else{
+
+        }
+        contInner.innerHTML = data;
+        container.appendChild(contInner);
+    }
+
+    contInner = document.createElement("td");
+
+    let link = "ViewPetsByOwner.html";
+
+    let modifyBtn = "<button class='btn btn-primary' onclick='openForm()'>Modify</button>";
+
+    contInner.innerHTML = modifyBtn;
+
+    container.appendChild(contInner);
+
+}
+
+
+function eClicked(form) {
 
     let formObject = {};
     for (let element of form.elements) {
@@ -14,8 +86,8 @@ function eClicked(form){
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+}
