0dece6c474a912e72bd655f3eba181ca2e055581
Ruairidh <ruairidht2@gmail.com>
2019-09-22 15:02:34 +0100

    search function modified for exercises

--- assets/js/Exercises.js
+++ assets/js/Exercises.js
@@ -152,22 +152,25 @@ function addExercise(data) {
     return false;
 }
 
-
 function searchFunction() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("tabBod");
     tr = table.getElementsByTagName("tr");
+
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[1];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+
+        td = tr[i].getElementsByTagName("td");
+
+        if (td.length > 0) { // to avoid th
+
+            if (td[0].innerHTML.toUpperCase().indexOf(filter) > -1 || td[1].innerHTML.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
             } else {
                 tr[i].style.display = "none";
             }
+
         }
     }
 }
